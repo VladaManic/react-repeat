56091b1a646333a911beada9f346e4387d276c89
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2022-06-22 15:29:01 +0200

    geting and adding to context

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,13 +1,26 @@
+import React, { useState, useContext } from 'react';
+import ItemsContext from '../context/items-context';
+
 const Add = () => {
+	const itemsCtx = useContext(ItemsContext);
+	const [title, setTitle] = useState('')
+	const [text, setText] = useState('')
+
+	const submit = (e) => {
+		e.preventDefault()
+		const item = {title, text}
+		itemsCtx.addItem(item)
+	}
+
 	return (
 		<div className='add'>
 			<h1>Add</h1>
-			<form>
+			<form onSubmit={submit}>
 				<label for="title">Title</label>
-				<input type="text" id="title" />
+				<input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
 				<br/>
 				<label for="textarea">Text</label>
-				<textarea id="textarea"></textarea>
+				<textarea id="textarea" value={text} onChange={(e) => setText(e.target.value)}></textarea>
 				<br/>
 				<input type="submit" value="Send" />
 			</form>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,20 @@
-import { useState } from 'react'
+import React, {useState, useContext, useEffect} from 'react'
+import ItemsContext from '../context/items-context'
 
 import Items from '../components/Items'
 
 const Home = () => {
-	const [items, setItems] = useState([
-    {
-      id: 1,
-			title: 'Lorem ipsum',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc efficitur mi quis ante laoreet, id ornare mi dapibus.',
-    },
-    {
-      id: 2,
-			title: 'Vestibulum ante',
-      text: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer ac purus eget urna placerat tempor eget at lorem.',
-    },
-    {
-      id: 3,
-			title: 'Nullam congue',
-      text: 'Nullam congue elementum risus et laoreet. Morbi consequat sapien magna, ac cursus justo convallis et.',
-    },
-  ])
+	const itemsCtx = useContext(ItemsContext)
+  console.log(itemsCtx.items);
+
+	useEffect(() => {
+		itemsCtx.getItems()
+	}, [itemsCtx])
 
 	return (
 		<div className='home'>
 			<h1>Home</h1>
-			<Items items={items} />
+			<Items items={itemsCtx.items} />
 		</div>
 	)
 }
